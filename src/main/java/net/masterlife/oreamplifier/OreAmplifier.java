5d1c906d5f087fdfa4d0aeca3bf10139e30d3969
MasterLife777 <konradplotka30@gmail.com>
2023-04-03 02:39:33 +0200

    Initial commit

---
+++ src/main/java/net/masterlife/oreamplifier/OreAmplifier.java
@@ -0,0 +1,15 @@
+package net.masterlife.oreamplifier;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OreAmplifier implements ModInitializer {
+	public static final String MOD_ID = "oreamplifier";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
