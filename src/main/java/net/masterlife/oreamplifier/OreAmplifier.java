1e5f29b3ff6aad50cebd43106c475d06720d226b
MasterLife777 <konradplotka30@gmail.com>
2023-04-03 03:13:12 +0200

    Added Piece Of Death

--- src/main/java/net/masterlife/oreamplifier/OreAmplifier.java
+++ src/main/java/net/masterlife/oreamplifier/OreAmplifier.java
@@ -2,6 +2,7 @@ package net.masterlife.oreamplifier;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.masterlife.oreamplifier.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,5 +12,7 @@ public class OreAmplifier implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/masterlife/oreamplifier/OreAmplifierClient.java
@@ -0,0 +1,10 @@
+package net.masterlife.oreamplifier;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OreAmplifierClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
