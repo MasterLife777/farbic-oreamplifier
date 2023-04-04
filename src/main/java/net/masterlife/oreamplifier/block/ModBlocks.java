31b6ce9cc2121ab158085430f3c579610d4afcc5
MasterLife777 <konradplotka30@gmail.com>
2023-04-04 03:12:31 +0200

    Added loot_tables and repices for Death Ore, Death Block and Piece of Death

--- src/main/java/net/masterlife/oreamplifier/block/ModBlocks.java
+++ src/main/java/net/masterlife/oreamplifier/block/ModBlocks.java
@@ -4,11 +4,13 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.masterlife.oreamplifier.OreAmplifier;
 import net.minecraft.block.Block;
+import net.minecraft.block.MapColor;
 import net.minecraft.block.Material;
 import net.minecraft.block.OreBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.util.registry.Registry;
@@ -16,9 +18,9 @@ import net.minecraft.util.registry.Registry;
 public class ModBlocks {
 
     public static final Block DEATH_BLOCK = registerBlock("death_block",
-            new Block(FabricBlockSettings.of(Material.METAL)));
+            new Block(FabricBlockSettings.of(Material.METAL, MapColor.DARK_RED).requiresTool().strength(5.0f, 6.0f).sounds(BlockSoundGroup.METAL)));
     public static final Block DEATH_ORE = registerBlock("death_ore",
-            new OreBlock(FabricBlockSettings.of(Material.STONE),
+            new OreBlock(FabricBlockSettings.of(Material.STONE).requiresTool().strength(3.0f, 3.0f),
                     UniformIntProvider.create(3, 7)));
 
     private static Block registerBlock(String name, Block block) {
